4fc5fe4d627056eb9c2f64083fd9a0e764907604
ZoeXFCE <132409356+ZoeXFCE@users.noreply.github.com>
2023-05-04 00:00:26 +0100

    Update archMiniInstaller.sh

--- archMiniInstaller.sh
+++ archMiniInstaller.sh
@@ -25,3 +25,19 @@ mv themes ~/.themes
 
 sudo systemctl disable lightdm.service
 mv startxatlogin.sh /etc/profile.d/startxatlogin.sh
+
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -S mangohud minecraft-launcher mugshot --noconfirm
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+cd
+mkdir Downloads Pictures Documents
+cd ~/Downloads
+~/.local/bin/gdown https://drive.google.com/u/0/uc?id=1zgBrmyiMpSfW9xsZ9A4ejvR0lktWlTRR&export=download
+7z x 08-07-22_linux_infinity_13-27.7z -o08-07-22_linux_infinity_13-27
+cd 08-07-22_linux_infinity_13-27
+./pull.sh
